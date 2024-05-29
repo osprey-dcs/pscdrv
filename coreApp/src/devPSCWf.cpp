mdavidsaver/pscdrv-dev

commit 336a1b07e8715b0f101a0d07ea2bf603e03638e3

change copyout() arg list

coreApp/src/devPSCWf.cpp
@@ -88,8 +88,7 @@ long read_wf_real(waveformRecord* prec)
 
         // HACK: we are copying integers into a double[]
         // this is safe so long as sizeof(T)<=sizeof(double)
-        size_t ncopied = priv->block->data.copyout_shape(prec->bptr, priv->offset, sizeof(T), skip, prec->nelm);
-        size_t nelem = ncopied;
+        size_t nelem = priv->block->data.copyout_shape(prec->bptr, priv->offset, sizeof(T), skip, prec->nelm);
 
         // step backwards since we are expanding the used size of the array
         for(size_t i=nelem; i; i--) {
@@ -121,7 +120,7 @@ long read_wf_bytes(waveformRecord* prec)
             return 0;
         }
 
-        size_t len = priv->block->data.copyout_shape(prec->bptr, priv->offset, prec->nelm, 0u, 1u);
+        size_t len = priv->block->data.copyout(prec->bptr, priv->offset, prec->nelm);
 
         prec->nord = len;
 

coreApp/src/psc/evbase.h
@@ -93,8 +93,8 @@ public:
 
     bool copyin(const void *buf, size_t offset, size_t len);
 
-    bool copyout(void *dest, size_t nbytes) const {
-        return copyout_shape(dest, 0u, nbytes, 0u, 1u)==1u;
+    bool copyout(void *dest, size_t offset, size_t nbytes) const {
+        return copyout_shape(dest, offset, nbytes, 0u, 1u)==1u;
     }
     // returns number of elements copied
     size_t copyout_shape(void *dest, size_t offset, size_t esize, size_t eskip, size_t ecount) const;

coreApp/src/devPSCSingle.cpp
@@ -69,7 +69,7 @@ void received_block(void* raw, Block* block)
         char bytes[sizeof(epicsUInt32)];
     } data;
 
-    if(priv->block->data.copyout(data.bytes, sizeof(data.bytes))) {
+    if(priv->block->data.copyout(data.bytes, 0, sizeof(data.bytes))) {
         return;
     }
 
@@ -80,7 +80,7 @@ void received_block(void* raw, Block* block)
     bool alreadyQueued = !priv->syncData.empty();
 
     priv->syncData.resize(block->data.size());
-    size_t actual = priv->block->data.copyout(&priv->syncData[0], priv->syncData.size());
+    size_t actual = priv->block->data.copyout(&priv->syncData[0], 0, priv->syncData.size());
     priv->syncData.resize(actual); // maybe downsize
 
     if(!alreadyQueued)

testApp/testValues.cpp
@@ -133,7 +133,7 @@ void test_dbuffer_contig()
 
         char out[12];
         memset(out, 0xfe, sizeof(out));
-        testOk1(!!B.copyout(out, sizeof(inp)));
+        testOk1(!!B.copyout(out, 0, sizeof(inp)));
 
         testOk1(memcmp(inp, out, sizeof(out))==0);
     }
@@ -179,7 +179,7 @@ void test_dbuffer_discontrig()
         const char expect[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
         char out[16];
         memset(out, 0xfe, sizeof(out));
-        testOk1(!!B.copyout(out, sizeof(out)));
+        testOk1(!!B.copyout(out, 0, sizeof(out)));
 
         testOk1(memcmp(expect, out, sizeof(expect))==0);
     }

osprey-dcs/pscdrv

commit 2e3e0d18ac97cf7af062c0929b0adc288fb3f621

fix read_wf_bytes()
broken by 336a1b07e8715b0f101a0d07ea2bf603e03638e3

coreApp/src/devPSCWf.cpp
@@ -146,7 +146,7 @@ long read_wf_bytes(waveformRecord* prec)
             return 0;
         }
 
-        size_t len = priv->block->data.copyout(prec->bptr, priv->offset, prec->nelm);
+        size_t len = priv->block->data.copyout_bytes(prec->bptr, priv->offset, prec->nelm);
 
         prec->nord = len;
 

coreApp/src/evbase.cpp
@@ -256,6 +256,13 @@ bool dbuffer::copyin(const void *buf, size_t offset, size_t len)
     return ptr.copy(len, const_cast<void*>(buf), false)==len;
 }
 
+size_t dbuffer::copyout_bytes(void *dest, size_t offset, size_t nbytes) const
+{
+    stride_ptr ptr(*this);
+    ptr.copy(offset, 0u, true); // skip
+    return ptr.copy(nbytes, dest, true);
+}
+
 size_t dbuffer::copyout_shape(void *rawdest, size_t ioffset, size_t esize, size_t iskip, size_t dskip, size_t ecount) const
 {
     const size_t total = size();

coreApp/src/psc/evbase.h
@@ -112,6 +112,14 @@ public:
     bool copyout(void *dest, size_t offset, size_t nbytes) const {
         return copyout_shape(dest, offset, nbytes, 0u, 0u, 1u)==1u;
     }
+    /** Copy out a variable number of contiguious bytes.
+     *
+     * @param dest Output pointer
+     * @param offset Input bytes to skip before first byte read
+     * @param nbytes Maximum number of byte to read.
+     * @return Number of bytes actually read
+     */
+    size_t copyout_bytes(void *dest, size_t offset, size_t nbytes) const;
     /** Copy out array.
      *
      * @param dest Output pointer